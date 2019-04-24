4dd3a57f253f47e1de5fa51a64175fc047f9f5a4
Jenning Chen <jenning@mit.edu>
2019-04-23 20:54:11 -0400

    implemented beginning UI

--- tracker/tracker.ino
+++ tracker/tracker.ino
@@ -1,4 +1,5 @@
 #include <SPI.h>
+#include <WiFi.h> //Connect to WiFi Network
 #include <TFT_eSPI.h>
 TFT_eSPI tft = TFT_eSPI();
 
@@ -8,11 +9,24 @@ TFT_eSPI tft = TFT_eSPI();
 #define PRESSED 0
 #define UNPRESSED 1
 
+char network[] = "MIT GUEST";
+char password[] = "";
+
+const char USER[] = "jenning";
+
+//Some constants and some resources:
+const int RESPONSE_TIMEOUT = 6000; //ms to wait for response from host
+
+const uint16_t IN_BUFFER_SIZE = 1000; //size of buffer to hold HTTP request
+const uint16_t OUT_BUFFER_SIZE = 1000; //size of buffer to hold HTTP response
+char request_buffer[IN_BUFFER_SIZE]; //char array buffer to hold HTTP request
+char response_buffer[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP response
+
 const uint8_t PIN_1 = 16; //button 1
 const uint8_t PIN_2 = 5; //button 2
-const char
-uint32_t primary_timer;
 
+
+uint8_t toggle;
 uint8_t state;
 uint8_t screen_color;
 
@@ -22,12 +36,30 @@ char select_char[] = "-"; // selection indicator variable
 void setup() {
   Serial.begin(115200);               // Set up serial port
   pinMode(PIN_1, INPUT_PULLUP);
-  primary_timer = micros();
   tft.init();  //init screen
   tft.setRotation(2); //adjust rotation
   tft.setTextSize(1); //default font size
   tft.fillScreen(TFT_BLACK); //fill background
   tft.setTextColor(TFT_GREEN, TFT_BLACK); //set col
+  WiFi.begin(network, password); //attempt to connect to wifi
+  uint8_t count = 0; //count used for Wifi check times
+  Serial.print("Attempting to connect to ");
+  Serial.println(network);
+  while (WiFi.status() != WL_CONNECTED && count < 12) {
+    delay(500);
+    Serial.print(".");
+    count++;
+  }
+  delay(2000);
+  if (WiFi.isConnected()) { //if we connected then print our IP, Mac, and SSID we're on
+    Serial.println("CONNECTED!");
+    Serial.println(WiFi.localIP().toString() + " (" + WiFi.macAddress() + ") (" + WiFi.SSID() + ")");
+    delay(500);
+  } else { //if we failed to connect just Try again.
+    Serial.println("Failed to Connect :/  Going to restart");
+    Serial.println(WiFi.status());
+    ESP.restart(); // restart the ESP (proper way)
+  }
   in_welcome = false;
 }
 
@@ -37,7 +69,16 @@ void welcome() {
   tft.drawString("Welcome!", 0, 1, 1);
   tft.drawString("Register new item", 10, 10, 1);
   tft.drawString("Track items", 10, 20, 1);
-  tft.drawString("View registered items", 10, 30, 1);
+  tft.drawString("View registered", 10, 30, 1);
+  tft.drawString("items", 10, 40, 1);
+  tft.drawString(select_char, 2, 10, 1);
+}
+
+void register_prompt() {
+  tft.fillScreen(TFT_BLACK); //fill background
+  tft.drawString("Registering: place", 0, 50, 1);
+  tft.drawString("your item next to me!", 0, 60, 1);
+  
 }
 
 void loop() {
@@ -49,13 +90,12 @@ void loop() {
       }
       toggle = digitalRead(PIN_1);
       if (toggle == PRESSED) {
-        tft.drawString(select_char, 10, 1);
+        register_prompt();
+        state = REGISTER;
       }
       break;
     case REGISTER:
       break;
   }
   
-  primary_timer = micros();
-
 }
